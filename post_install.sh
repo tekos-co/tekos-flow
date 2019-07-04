8ac366b6f72d4ab6553a3bc2edd887ea46d3b890
haynajjar <najjar.abdelhay@gmail.com>
2019-07-04 13:53:04 +0100

    update post install

--- post_install.sh
+++ post_install.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/tekos-flow
 npm i
 npm i -g pm2
 echo "npm start" >> start.sh
-echo "export FLOW_LOGIN=admin\nexport FLOW_PASSWORD=admin" >> .bashrc
-source .bashrc
+echo "export FLOW_LOGIN=admin" >> /root/.bashrc
+echo "export FLOW_PASSWORD=admin" >> /root/.bashrc
+source /root/.bashrc
