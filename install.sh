d62861e8e4a2bd288c1c05026385f2e8e25aeff5
milede <miled.essaafi@gmail.com>
2019-07-04 18:16:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,4 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm install 12.4.0
 rm -rf tekos-flow
-git clone https://github.com/tekos-co/tekos-flow.git
-apt-get install python2.7    
 
-
-cd /home/ubuntu/tekos-flow
-npm i
-npm i -g pm2
-echo "npm start" >> start.sh
-echo "export FLOW_LOGIN=admin" >> /root/.bashrc
-echo "export FLOW_PASSWORD=admin" >> /root/.bashrc
-source /root/.bashrc
-pm2 start /home/ubuntu/tekos-flow/start.sh --name tekos-flow
