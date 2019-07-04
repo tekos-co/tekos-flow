873896d819627f7ab1c6bd2e35624df52c2250c5
milede <miled.essaafi@gmail.com>
2019-07-04 18:07:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ apt update
 
 cd /home/ubuntu
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source /root/.bashrc 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 12.4.0
 rm -rf tekos-flow
 git clone https://github.com/tekos-co/tekos-flow.git
