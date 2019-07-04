34a0c2239d903ec5a1a79538699bd5b1edcd4e9d
milede <miled.essaafi@gmail.com>
2019-07-04 18:30:38 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,7 @@
 # git clone https://github.com/tekos-co/tekos-flow.git
 apt-get install python2.7    
 cd /home/ubuntu/tekos-flow
+alias npm="/root/.nvm/versions/node/v12.4.0/bin/npm"
 npm i
 npm i -g pm2
 echo "npm start" >> start.sh
