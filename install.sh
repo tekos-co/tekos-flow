d2796cccab6adbe77c64ee2560f2fce170f88a9e
milede <miled.essaafi@gmail.com>
2019-07-04 17:26:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd /home/ubuntu
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source .bashrc 
 nvm install 12.4.0
+rm -rf tekos-flow
 git clone git@bitbucket.org:tekos2/tekos-flow.git
 apt-get install python2.7    
 
