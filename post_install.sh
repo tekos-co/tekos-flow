aee9915b4abb6e3f7e90376c492859f12815d85e
milede <miled.essaafi@gmail.com>
2019-07-04 18:17:59 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# git clone https://github.com/tekos-co/tekos-flow.git
+apt-get install python2.7    
 cd /home/ubuntu/tekos-flow
 npm i
 npm i -g pm2
 echo "npm start" >> start.sh
-echo "export FLOW_LOGIN=admin" >> /root/.bashrc
-echo "export FLOW_PASSWORD=admin" >> /root/.bashrc
-source /root/.bashrc
+export FLOW_LOGIN=admin
+export FLOW_PASSWORD=admin
+
 pm2 start /home/ubuntu/tekos-flow/start.sh --name tekos-flow
