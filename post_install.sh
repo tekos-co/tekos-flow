151c63922f3ec1ea0d2d8b687a738d8322c109bc
milede <miled.essaafi@gmail.com>
2019-07-05 10:27:38 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -10,7 +10,5 @@ cd /home/ubuntu/tekos-flow
 npm i
 npm i -g pm2
 echo "npm start" >> start.sh
-export FLOW_LOGIN=admin
-export FLOW_PASSWORD=admin
 
-pm2 start /home/ubuntu/tekos-flow/start.sh --name tekos-flow
+
