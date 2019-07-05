d36d98bc27286e10e093a50304f07c277569aabc
milede <miled.essaafi@gmail.com>
2019-07-05 09:15:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+pm2 delete -s tekos-flow || :
 pm2 start -f /home/ubuntu/tekos-flow/start.sh --name tekos-flow
