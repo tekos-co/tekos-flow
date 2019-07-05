2308a88b2b847701d549adbb600b7644a8e0fa5e
milede <miled.essaafi@gmail.com>
2019-07-05 08:23:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-alias pm2="/root/.nvm/versions/node/v12.4.0/bin/pm2"
-pm2 start /home/ubuntu/tekos-flow/start.sh --name tekos-flow
+/root/.nvm/versions/node/v12.4.0/bin/pm2 start /home/ubuntu/tekos-flow/start.sh --name tekos-flow
