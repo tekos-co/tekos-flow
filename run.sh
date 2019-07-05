f2e5545be18f4e59ad780d7fa0d249caa9078eb1
milede <miled.essaafi@gmail.com>
2019-07-05 10:27:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+export FLOW_LOGIN=admin
+export FLOW_PASSWORD=admin
+
 cd /home/ubuntu/tekos-flow
 pm2 delete -s tekos-flow || :
 pm2 start -f start.sh --name tekos-flow
