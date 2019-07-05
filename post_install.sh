413b41c68b4b785d21a2a6b8215169c2ae9deade
milede <miled.essaafi@gmail.com>
2019-07-05 09:00:33 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,7 @@
 source /root/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # git clone https://github.com/tekos-co/tekos-flow.git
 apt-get install python2.7    
